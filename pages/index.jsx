013518f6c9c18150ddc285bd5d6c0ce8158944e3
tommymdaniel <tommymdaniel@gmail.com>
2024-06-23 11:37:59 -0400

    update to index.js file title

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ export default function Index({ allPosts }) {
     <>
       <Layout>
         <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+          <title>{`Lunect Holdings`}</title>
         </Head>
         <Container>
           <Intro />
